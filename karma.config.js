db4b7fa27463b41c797c0a69ba553c4ff5ffa372
vani_junying@hotmail.com <vali@SSC1-W257.schenker-asia.com>
2015-10-30 15:15:26 +0800

    add more libs and done for chapter10

---
+++ karma.config.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: vali
+ * Date: 15-10-30
+ * Time: 上午10:53
+ * To change this template use File | Settings | File Templates.
+ */
+module.exports=function(config){
+       config.set(
+
+       )
+}
